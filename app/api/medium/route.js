a1c2628fc976bbba3d38101ceaa80d05bf643e47
harshhj17 <harshjaiswal857@gmail.com>
2025-04-14 12:29:49 +0530

    final changes

--- app/api/medium/route.js
+++ app/api/medium/route.js
@@ -2,7 +2,7 @@ import Parser from 'rss-parser';
 
 export async function GET() {
     const parser = new Parser();
-    const mediumFeed = await parser.parseURL('https://medium.com/feed/@harshjaiswal857');
+    const mediumFeed = await parser.parseURL('https://medium.com/feed/@harshjaiswal2001');
 
     const posts = mediumFeed.items.slice(0, 5).map((item) => ({
         title: item.title || '',
